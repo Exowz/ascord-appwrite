706740a6e1fc9476b6ca2b63beb290c497ea61b1
Exowz <53607938+Exowz@users.noreply.github.com>
2024-12-13 02:36:41 +0100

    changelog updated

--- src/features/landing/changelog.tsx
+++ src/features/landing/changelog.tsx
@@ -16,28 +16,28 @@ const Changelog = () => {
               </p>
               <div className="grid grid-cols-2 gap-4">
                 <Image
-                  src="https://assets.aceternity.com/templates/startup-1.webp"
+                  src="https://raw.githubusercontent.com/Exowz/ascord-appwrite/refs/heads/main/src/assets/images/timeline-pics/after/Hero%402x.png"
                   alt="startup template"
                   width={500}
                   height={500}
                   className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
                 />
                 <Image
-                  src="https://assets.aceternity.com/templates/startup-2.webp"
+                  src="https://raw.githubusercontent.com/Exowz/ascord-appwrite/refs/heads/main/src/assets/images/timeline-pics/after/auth%20page%402x.png"
                   alt="startup template"
                   width={500}
                   height={500}
                   className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
                 />
                 <Image
-                  src="https://assets.aceternity.com/templates/startup-3.webp"
+                  src="https://raw.githubusercontent.com/Exowz/ascord-appwrite/refs/heads/main/src/assets/images/timeline-pics/after/auth%20page%402x.png"
                   alt="startup template"
                   width={500}
                   height={500}
                   className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
                 />
                 <Image
-                  src="https://assets.aceternity.com/templates/startup-4.webp"
+                  src="https://raw.githubusercontent.com/Exowz/ascord-appwrite/refs/heads/main/src/assets/images/timeline-pics/after/Edit%20Workspace.png"
                   alt="startup template"
                   width={500}
                   height={500}
@@ -72,7 +72,7 @@ const Changelog = () => {
                   className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
                 />
                 <Image
-                  src="https://raw.githubusercontent.com/Exowz/ascord-appwrite/refs/heads/main/src/assets/images/timeline-pics/before/IMG-20240705-WA0029.jpg"
+                  src="https://raw.githubusercontent.com/Exowz/ascord-appwrite/refs/heads/main/src/assets/images/timeline-pics/after/Workspace.png"
                   alt="bento template"
                   width={500}
                   height={500}
@@ -111,30 +111,30 @@ const Changelog = () => {
                 </div>
               </div>
               <div className="grid grid-cols-2 gap-4">
-                <Image
-                  src="https://assets.aceternity.com/pro/hero-sections.png"
-                  alt="hero template"
+              <Image
+                  src="https://raw.githubusercontent.com/Exowz/ascord-appwrite/refs/heads/main/src/assets/images/timeline-pics/after/Hero%402x.png"
+                  alt="startup template"
                   width={500}
                   height={500}
                   className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
                 />
                 <Image
-                  src="https://assets.aceternity.com/features-section.png"
-                  alt="feature template"
+                  src="https://raw.githubusercontent.com/Exowz/ascord-appwrite/refs/heads/main/src/assets/images/timeline-pics/after/auth%20page%402x.png"
+                  alt="startup template"
                   width={500}
                   height={500}
                   className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
                 />
                 <Image
-                  src="https://assets.aceternity.com/pro/bento-grids.png"
-                  alt="bento template"
+                  src="https://raw.githubusercontent.com/Exowz/ascord-appwrite/refs/heads/main/src/assets/images/timeline-pics/after/Workspace.png"
+                  alt="startup template"
                   width={500}
                   height={500}
                   className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
                 />
                 <Image
-                  src="https://assets.aceternity.com/cards.png"
-                  alt="cards template"
+                  src="https://raw.githubusercontent.com/Exowz/ascord-appwrite/refs/heads/main/src/assets/images/timeline-pics/after/Edit%20Workspace.png"
+                  alt="startup template"
                   width={500}
                   height={500}
                   className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
